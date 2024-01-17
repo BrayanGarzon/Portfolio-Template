3ad711a500f1efa84065354c96e44b54444e0afa
Brayan Garzon <destroyergarzon@gmail.com>
2024-01-16 21:16:46 -0500

    add section aboutMe

--- js/index.js
+++ js/index.js
@@ -8,3 +8,33 @@ window.addEventListener('DOMContentLoaded', function() {
       });
   });
   
+
+  /* FUNTION ICON SCROLL IN THE INDEX */
+
+window.addEventListener('DOMContentLoaded', function(){
+  fetch('iconScroll.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('scroll-container').
+    innerHTML = data;
+  });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var scrollContainer = document.getElementById('scroll-container');
+  
+  window.addEventListener('scroll', function() {
+      var scrollPosition = window.scrollY;
+
+      // Ajusta este valor según sea necesario para determinar cuándo quieres que el div se oculte
+      var scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+          scrollContainer.classList.add('scroll-hidden');
+      } else {
+          scrollContainer.classList.remove('scroll-hidden');
+      }
+  });
+});
