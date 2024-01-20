6c6dcf8b900158f4a9049ce62e331897a6d36f41
Brayan Garzon <destroyergarzon@gmail.com>
2024-01-19 20:14:31 -0500

    add section projects

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ window.addEventListener('DOMContentLoaded', function() {
   });
   
 
+
   /* FUNTION ICON SCROLL IN THE INDEX */
 
 window.addEventListener('DOMContentLoaded', function(){
@@ -21,14 +22,14 @@ window.addEventListener('DOMContentLoaded', function(){
 });
 
 
-
+  /* FUNTION ICON SCROLL CHANGE DISPLAY NONE */
 document.addEventListener('DOMContentLoaded', function() {
   var scrollContainer = document.getElementById('scroll-container');
   
   window.addEventListener('scroll', function() {
       var scrollPosition = window.scrollY;
 
-      // Ajusta este valor según sea necesario para determinar cuándo quieres que el div se oculte
+      
       var scrollThreshold = 100;
 
       if (scrollPosition > scrollThreshold) {
@@ -38,3 +39,47 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   });
 });
+
+
+
+
+
+/* FOOTER IMPORT */
+window.addEventListener('DOMContentLoaded', function() {
+  fetch('footer.html')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('sectionFooter').innerHTML = data;
+
+      // Luego de cargar el HTML del pie de página, actualiza el año
+      updateYear();
+    })
+    .catch(error => {
+      console.error('Error fetching footer.html:', error);
+    });
+});
+
+
+/* FUNTION YEAR OF THE FOOTER */
+function updateYear() {
+  // Obtén el elemento del año actual
+  var currentYearElement = document.getElementById("currentYear");
+
+  if (currentYearElement) {
+    // Obtén el año actual
+    var currentYear = new Date().getFullYear();
+
+    // Actualiza el contenido del elemento con el año actual
+    currentYearElement.textContent = currentYear.toString();
+  } else {
+    console.error("El elemento con ID 'currentYear' no fue encontrado en el pie de página.");
+  }
+}
+
+// Llama a la función de actualización del año cuando el DOM está completamente cargado
+document.addEventListener("DOMContentLoaded", updateYear);
